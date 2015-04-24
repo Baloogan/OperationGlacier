1f8be1b2f7d214ddf65c9e125e1bf79917fec374
Rory <baloogan@gmail.com>
2015-04-24 13:32:05 -0700

    Initial Commit.

---
+++ OperationGlacier/Controllers/MapController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OperationGlacier.Controllers
+{
+    public class MapController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
