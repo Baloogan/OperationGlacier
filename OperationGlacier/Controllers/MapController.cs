fd296ea099c112d9bbabc14db66555bca030c1ca
Rory Anderson <baloogan@gmail.com>
2015-05-03 07:00:43 -0700

    Team security now matters! Folks can lock themselves to a team.

--- OperationGlacier/Controllers/MapController.cs
+++ OperationGlacier/Controllers/MapController.cs
@@ -3,19 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using Microsoft.AspNet.Identity;
+using OperationGlacier.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
 namespace OperationGlacier.Controllers
 {
     public class MapController : Controller
     {
+        /// <summary>
+        /// Application DB context
+        /// </summary>
+        protected ApplicationDbContext ApplicationDbContext { get; set; }
+
+        /// <summary>
+        /// User manager - attached to application DB context
+        /// </summary>
+        protected UserManager<ApplicationUser> UserManager { get; set; }
+
+        public MapController()
+        {
+            this.ApplicationDbContext = new ApplicationDbContext();
+            this.UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(this.ApplicationDbContext));
+        }
+
         public ActionResult Index(string side, string date)
         {
             ViewBag.Side = side;
             ViewBag.Date = date;
-            if (side != "Allies" && System.Web.HttpContext.Current.User.Identity.Name == "baloogan@gmail.com")
-                return View("Index", "Home");
-            if (side != "Japan" && System.Web.HttpContext.Current.User.Identity.Name == "historicalgamer@gmail.com")
-                return View("Index", "Home");
+
+            var user = UserManager.FindById(User.Identity.GetUserId());
+            if (user.SideRestriction != "Both")
+            {
+                if (user.SideRestriction != side)
+                {
+                    return new RedirectResult("~/");
+                }
+            }
             return View();
         }
 
