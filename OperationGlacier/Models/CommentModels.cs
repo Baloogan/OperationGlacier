1c058711d809e6a7af23949f015f10c2a32f6b01
Rory Anderson <baloogan@gmail.com>
2015-05-03 21:44:03 -0700

    Switched to VS2013, made some changes to the index page.

---
+++ OperationGlacier/Models/CommentModels.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace OperationGlacier.Models
+{
+    public class CommentModel
+    {
+        public string Username { get; set; }
+    }
+}
